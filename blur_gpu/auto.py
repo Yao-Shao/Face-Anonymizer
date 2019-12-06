fc91f128c4d3c45ee9277d4f1321f32d99aa25a4
Yao Shao <yshao998@gmail.com>
2019-12-05 16:33:22 -0800

    first cm

---
+++ blur_gpu/auto.py
@@ -0,0 +1,31 @@
+# 1. source /media/msc/Elements/Research/self-driving/Anonymizer/blur_gpu/myvenv/bin/activate
+# 2. python3 auto.py
+
+import os
+
+cd = 'cd /media/msc/Elements/Research/self-driving/Anonymizer/blur_gpu/anonymizer'
+os.system(cd)
+
+# act = 'source ./myvenv/bin/activate'
+# os.system(act)
+
+#cd2 = 'cd anonymizer'
+#os.system(cd2)
+
+bag_name = r'autoware-20190828184706'   
+base_path = r'/media/msc/Elements/Research/self-driving/Anonymizer/bags'
+exe = r'PYTHONPATH=$PYTHONPATH:/media/msc/Elements/Research/self-driving/Anonymizer/blur_gpu/anonymizer python /media/msc/Elements/Research/self-driving/Anonymizer/blur_gpu/anonymizer/anonymizer/bin/anonymize.py'
+my_input = r' --input ' + base_path + '/' + bag_name + '/cam'
+my_output = r' --image-output ' + base_path + '/' + bag_name + '_blur/cam' 
+my_weight = r' --weights /media/msc/Elements/Research/self-driving/Anonymizer/blur_gpu/anonymizer/weight --no-write-detections'
+
+if not os.path.exists(base_path + '/' + bag_name + '_blur'):
+	os.mkdir(base_path + '/' + bag_name + '_blur')
+	print('create new folder at {}'.format(base_path + '/' + bag_name + '_blur'))
+
+for i in range(0,1):
+	if not os.path.exists(base_path + '/' + bag_name + '_blur/cam' + str(i)):
+		os.mkdir(base_path + '/' + bag_name + '_blur/cam' + str(i))
+
+	job = exe + my_input + str(i) + my_output + str(i) + my_weight
+	os.system(job)
---
+++ write_bag/write_bag.py
@@ -0,0 +1,74 @@
+# cv2 4.1.2.30 required
+
+import rosbag
+import os
+import cv2
+from cv_bridge import CvBridge
+from sensor_msgs.msg import Image
+from tqdm import *
+
+######## config #######
+BAG_NAME = r'autoware-20190828191451'
+BAG_PATH_OLD = r'/media/cyrus/Elements/Research/self-driving/Anonymizer/bags/' + BAG_NAME + '.bag'
+BAG_PATH_NEW = r'/media/cyrus/Elements/Research/self-driving/Anonymizer/bags/' + BAG_NAME + '_blur.bag'
+PIC_PATH_BLURRED = r'/media/cyrus/Elements/Research/self-driving/Anonymizer/bags/' + BAG_NAME + '_blur'
+#######################
+
+camera_list= ['cam0','cam1','cam2','cam3','cam4','cam5']
+
+def get_cam_path():
+	res = []
+	for i,cam in enumerate(camera_list):
+		p = PIC_PATH_BLURRED + '/' + cam
+		pp = os.listdir(p)
+		pp.sort()
+		ppp = [p + '/' + i for i in pp]
+		# print(ppp)
+		res.append(ppp)
+	return res
+
+with rosbag.Bag(BAG_PATH_NEW, 'w') as outbag:
+	cp = get_cam_path()
+	cnt = [0,0,0,0,0,0]
+	print('loading bag...')
+	inbag = rosbag.Bag(BAG_PATH_OLD)
+	print('loading success!')
+	num = inbag.get_message_count()
+	br = CvBridge()
+	with tqdm(total=num) as pbar:
+		for topic, msg, t in inbag.read_messages():
+			if topic == '/camera_array/cam0/image_raw/compressed':
+				cv_img = cv2.imread(cp[0][cnt[0]])
+				img_msg = br.cv2_to_compressed_imgmsg(cv_img, dst_format='jpg')
+				outbag.write(topic, img_msg, msg.header.stamp if msg._has_header else t)
+				cnt[0] += 1
+			elif topic == '/camera_array/cam1/image_raw/compressed':
+				cv_img = cv2.imread(cp[1][cnt[1]])
+				img_msg = br.cv2_to_compressed_imgmsg(cv_img, dst_format='jpg')
+				outbag.write(topic, img_msg, msg.header.stamp if msg._has_header else t)
+				cnt[1] += 1
+			elif topic == '/camera_array/cam2/image_raw/compressed':
+				cv_img = cv2.imread(cp[2][cnt[2]])
+				img_msg = br.cv2_to_compressed_imgmsg(cv_img, dst_format='jpg')
+				outbag.write(topic, img_msg, msg.header.stamp if msg._has_header else t)
+				cnt[2] += 1
+			elif topic == '/camera_array/cam3/image_raw/compressed':
+				# print(cp[3][cnt[3]])
+				cv_img = cv2.imread(cp[3][cnt[3]])
+				img_msg = br.cv2_to_compressed_imgmsg(cv_img, dst_format='jpg')
+				outbag.write(topic, img_msg, msg.header.stamp if msg._has_header else t)
+				cnt[3] += 1
+			elif topic == '/camera_array/cam4/image_raw/compressed':
+				cv_img = cv2.imread(cp[4][cnt[4]])
+				img_msg = br.cv2_to_compressed_imgmsg(cv_img, dst_format='jpg')
+				outbag.write(topic, img_msg, msg.header.stamp if msg._has_header else t)
+				cnt[4] += 1
+			elif topic == '/camera_array/cam5/image_raw/compressed':
+				cv_img = cv2.imread(cp[5][cnt[5]])
+				# print(type(cv_img))
+				img_msg = br.cv2_to_compressed_imgmsg(cv_img, dst_format='jpg')
+				outbag.write(topic, img_msg, msg.header.stamp if msg._has_header else t)
+				cnt[5] += 1
+			else:
+				outbag.write(topic, msg, msg.header.stamp if msg._has_header else t)
+			pbar.update()
